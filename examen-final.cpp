3407b261531733b02ebb8f2b066b4b2ea02e1ec6
Ariany <arianylopez341@gmail.com>
2024-06-06 16:15:28 -0700

    Creacion de estructura y cuerpo de main

---
+++ examen-final.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Date{
+    int year;
+    int month;
+    int day;
+};
+
+Date date;
+map<string, set<string>> events; 
+
+int main(){
+
+    return 0;
+}
