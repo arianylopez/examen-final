968469801fcaf1eedee04f697a432a843c4b3b84
Ariany <arianylopez341@gmail.com>
2024-06-06 21:49:54 -0700

    Añadiendo funcion Delete Event

--- examen-final.cpp
+++ examen-final.cpp
@@ -16,6 +16,7 @@ Date date;
 map<string, set<string>> events; 
 
 void add_event(map<string, set<string>>& events, const string& date_str, const string& event);
+bool delete_event(map<string, set<string>>& events, const string& date_str, const string& event);
 
 int main(){
     string operation;
@@ -28,4 +29,15 @@ int main(){
 
 void add_event(map<string, set<string>>& events, const string& date_str, const string& event){
     events[date_str].insert(event);
+}
+
+bool delete_event(map<string, set<string>>& events, const string& date_str, const string& event){
+    if(events.count(date_str) && events[date_str].count(event)){
+        events[date_str].erase(event);
+        if(events[date_str].empty()){
+            events.erase(date_str);
+        }
+        return true;
+    }
+    return false;
 }
