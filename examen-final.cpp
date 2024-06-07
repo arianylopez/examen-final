5b20b1b0d859926e8a3a0f8c0ad2f2662ca70992
Ariany <arianylopez341@gmail.com>
2024-06-06 18:26:32 -0700

    Agregando funcion Add Event

--- examen-final.cpp
+++ examen-final.cpp
@@ -15,7 +15,17 @@ struct Date{
 Date date;
 map<string, set<string>> events; 
 
+void add_event(map<string, set<string>>& events, const string& date_str, const string& event);
+
 int main(){
+    string operation;
+    string date_str;
+    string event;
+    string error_msg;
 
     return 0;
+}
+
+void add_event(map<string, set<string>>& events, const string& date_str, const string& event){
+    events[date_str].insert(event);
 }
