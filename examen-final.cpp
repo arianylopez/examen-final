7ea8fcdc8dccf64fcc16046565490fb5f9101055
Ariany <arianylopez341@gmail.com>
2024-06-07 01:40:29 -0700

    Añadiendo funcion Validate date

--- examen-final.cpp
+++ examen-final.cpp
@@ -17,6 +17,7 @@ map<string, set<string>> events;
 
 void add_event(map<string, set<string>>& events, const string& date_str, const string& event);
 bool delete_event(map<string, set<string>>& events, const string& date_str, const string& event);
+bool validate_date(const string& date_str, string& error_msg);
 
 int main(){
     string operation;
@@ -40,4 +41,41 @@ bool delete_event(map<string, set<string>>& events, const string& date_str, cons
         return true;
     }
     return false;
+}
+
+bool validate_date(const string& date_str, string& error_msg){
+    vector<int> date_parts;
+    string part;
+
+    for(char c : date_str){
+        if(c=='-'){
+            date_parts.push_back(stoi(part));
+            part.clear();
+        } else{
+            part += c;
+        }
+    }
+    date_parts.push_back(stoi(part));
+
+    if(date_parts.size() != 3){
+        error_msg="Wrong date format: " + date_str;
+        return false;
+    }
+
+    date.year=date_parts[0];
+    date.month=date_parts[1];
+    date.day=date_parts[2];
+
+    if(date.month<1 || date.month>12){
+        error_msg="Month value is invalid" + to_string(date.month);
+        return false;
+    }
+
+    vector<int> days_in_month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    if(date.day<1 || date.day>days_in_month[date.month - 1]){
+        error_msg = "Day value is invalid: " + to_string(date.day);
+        return false;
+    }
+    return true;
 }
