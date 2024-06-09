ecf5ef6736831257863cf72f9fc74937250b9cf1
Ariany <arianylopez341@gmail.com>
2024-06-08 18:25:49 -0700

    Modularidad

--- examenfinal.cpp
+++ examenfinal.cpp
@@ -3,77 +3,10 @@
 #include <set>
 #include <string>
 #include <vector>
-
-using namespace std;
-
-struct Date{
-    int year;
-    int month;
-    int day;
-};
+#include "examenfinal.h"
 
 Date date;
 map<string, set<string>> events; 
-
-void add_event(map<string, set<string>>& events, const string& date_str, const string& event);
-bool delete_event(map<string, set<string>>& events, const string& date_str, const string& event);
-bool validate_date(const string& date_str, string& error_msg);
-int delete_date(map<string, set<string>>& events, const string& date_str);
-set<string> find(map<string, set<string>>& events, const string& date_str);
-void print_events();
-
-int main(){
-    string operation;
-    string date_str;
-    string event;
-    string error_msg;
-
-    while(cin>>operation){
-        if(operation=="Add"){
-            cin>>date_str;
-            getline(cin, event);
-            event=event.substr(1);
-            if(validate_date(date_str, error_msg)){
-                add_event(events, date_str, event);
-            } else{
-                cout<<error_msg<<endl;
-            }
-        } else if(operation=="Del"){
-            cin>>date_str;
-            getline(cin, event);
-            if(validate_date(date_str, error_msg)){
-                if(event.empty()){
-                    cout<<"Deleted "<<delete_date(events, date_str)<<" events"<<endl;
-                } else{
-                    event=event.substr(1);
-                    if(delete_event(events, date_str, event)){
-                        cout<<"Deleted successfully"<<endl;
-                    } else{
-                        cout<<"Event not found"<<endl;
-                    }
-                }
-            } else{
-                cout<<error_msg<<endl;
-            }
-        } else if(operation=="Find"){
-            cin>>date_str;
-            if(validate_date(date_str, error_msg)){
-                set<string> events_found=find(events, date_str);
-                for(const auto& event:events_found){
-                    cout<<event<<endl;
-                }
-            } else{
-                cout<<error_msg<<endl;
-            }
-        } else if(operation=="Print"){
-            print_events();
-        } else{
-            cout<<"Unknown command: "<<operation<<endl;
-        }
-    }
-    
-    return 0;
-}
 
 void add_event(map<string, set<string>>& events, const string& date_str, const string& event){
     events[date_str].insert(event);
